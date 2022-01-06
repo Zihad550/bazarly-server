2c2be8bfb8be1c5d84d32a3ce458bf1e34f80c89
Zihad550 <hussainmuzahid83@gmail.com>
2022-01-06 16:00:11 +0600

    initial work done

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const ObjectId = require("mongodb").ObjectId;
+
+const port = process.env.PORT || 8000;
+
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+// mongo client
+const client = new MongoClient(process.env.URI);
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("bazarly");
+    const catagoriesCollection = database.collection("catagories");
+    const shoesCollection = database.collection("shoes");
+    const brandsCollection = database.collection("brands");
+
+    // get catagories
+    app.get("/catagories", async (req, res) => {
+      const result = await catagoriesCollection.find({}).toArray();
+      res.json(result);
+    });
+
+    // get shoes
+    app.get("/shoes", async (req, res) => {
+      const result = await shoesCollection.find({}).toArray();
+      res.json(result);
+    });
+
+    // get brands
+    app.get("/brands", async (req, res) => {
+      const result = await brandsCollection.find({}).toArray();
+      res.json(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to green bazarly server");
+});
+
+app.listen(port, () => {
+  console.log("port running at localhost:", port);
+});
